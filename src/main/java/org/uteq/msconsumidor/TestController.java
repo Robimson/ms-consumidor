6f11bd3f181c7cf8f3975d26af8cef2f2c8543dc
Robinson <rcandom@uteq.edu.ec>
2026-03-02 16:31:15 -0500

    Agregando interfaz de usuario al consumidor

--- src/main/java/org/uteq/msconsumidor/TestController.java
+++ src/main/java/org/uteq/msconsumidor/TestController.java
@@ -3,20 +3,16 @@ package org.uteq.msconsumidor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class TestController {
 
-    @GetMapping("/probar-conexion")
-    public String probar() {
+    @GetMapping("/api/consumir-datos")
+    public String obtenerDatos() {
         RestTemplate restTemplate = new RestTemplate();
         String url = "http://ms-usuarios-service/api/entregas";
-
-        try {
-            String response = restTemplate.getForObject(url, String.class);
-            return "Conexión Exitosa. El Microservicio Principal respondió: " + response;
-        } catch (Exception e) {
-            return "Error al conectar con el Microservicio Principal: " + e.getMessage();
-        }
+        return restTemplate.getForObject(url, String.class);
     }
 }
