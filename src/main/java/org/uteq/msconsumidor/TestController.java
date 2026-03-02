8a3db8036e54d0d7c1848a7240b6ad05125eb0c3
Robinson <rcandom@uteq.edu.ec>
2026-03-02 17:19:44 -0500

    presentacion

--- src/main/java/org/uteq/msconsumidor/TestController.java
+++ src/main/java/org/uteq/msconsumidor/TestController.java
@@ -9,20 +9,15 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin(origins = "*")
 public class TestController {
 
-    @GetMapping("/")
-    public String home() {
-        return "Servidor Consumidor Activo. <br> Entra a <a href='/index.html'>/index.html</a> para ver la interfaz.";
-    }
 
     @GetMapping("/api/consumir-datos")
     public String obtenerDatos() {
         RestTemplate restTemplate = new RestTemplate();
-        // Usamos el nombre del servicio del Microservicio A
         String url = "http://ms-usuarios-service/api/entregas";
         try {
             return restTemplate.getForObject(url, String.class);
         } catch (Exception e) {
-            return "{\"error\": \"No se pudo conectar con el Microservicio Principal: " + e.getMessage() + "\"}";
+            return "{\"error\": \"No se pudo conectar: " + e.getMessage() + "\"}";
         }
     }
 }
